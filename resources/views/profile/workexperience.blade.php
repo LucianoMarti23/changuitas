c7af4ea0f6789e0e57b042ca923017511bb35ef0
LucianoDiEco <111542636+LucianoDiEco@users.noreply.github.com>
2024-10-21 23:12:57 -0300

    arreglo

--- resources/views/profile/workexperience.blade.php
+++ resources/views/profile/workexperience.blade.php
@@ -8,7 +8,7 @@
 <!-- Botón para abrir el modal -->
 <div class="flex justify-end mb-6">
     <button id="openModalBtn" class="btn-success btn-standard text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">
-       Agregar Experiencia
+       Agregar Experienciass
     </button>
 </div>
 
